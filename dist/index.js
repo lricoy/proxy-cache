4aea9890fa83a55b3df03a2da8f6393d127165ff
Lucas Ricoy <ricoy.lucas@gmail.com>
2016-02-16 00:09:35 -0200

    feat(bower): Building the files before commit to use with bower

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var _cache = require('./cache');
+
+var _proxy = require('./proxy');
+
+var proxyCount = 0;
+var proxies = {};
+
+module.exports = {
+    Proxy: _proxy.Proxy,
+    Cache: _cache.Cache
+};
