f08eb37c66bd33e196667d39a51f1de68f976970
Lucas Ricoy <ricoy.lucas@gmail.com>
2016-02-22 18:47:30 -0300

    style(Tests): Using describes to separate methods, attrs, etc...

--- tests/index.test.js
+++ tests/index.test.js
@@ -22,114 +22,136 @@ describe('Cache', function () {
         expect(cache.getObjList().length).to.equal(0);
     });
 
-    it('should be an object', function(){
+    it('should be an object', () => {
         expect(cache).to.be.a('object');
     });
 
-    it('should instantiate without the need to use `new`', function(){
+    it('should instantiate without the need to use `new`', () => {
         expect(Cache()).to.be.instanceOf(Cache);
     });
 
-    it('should start with a empty list', function(){
+    it('should start with a empty list', () => {
         expect(cache.getObjList()).to.be.a('array');
         expect(cache.getObjList().length).to.equal(0);
     });
 
-    it('should start with a empty hash', function(){
+    it('should start with a empty hash', () => {
         expect(cache.getObjHash()).to.be.a('object');
         expect(Object.keys(cache.getObjHash()).length).to.equal(0);
     });
 
-    it('should cache a new object to the list and hash', function(){
-        cache.syncObj({_id: 1});
-        expect(cache.getObjList().length).to.equal(1);
-        expect(cache.getObj(1)._id).to.equal(1);
-        expect(Object.keys(cache.getObjHash()).length).to.equal(1);
-    });
-
-    it('should not append the same object to the list and hash', function(){
-        cache.syncObj({_id: 1});
-        expect(cache.getObjList().length).to.equal(1);
-        expect(cache.getObj(1)._id).to.equal(1);
-
-        cache.syncObj({_id: 1, name:'Lucas'});
-        expect(cache.getObjList().length).to.equal(1);
-        expect(cache.getObj(1)._id).to.equal(1);
-        expect(cache.getObj(1).name).to.equal('Lucas');
-        expect(Object.keys(cache.getObjHash()).length).to.equal(1);
-    });
-
-    it('should remove an object from the list and hash', function(){
-        cache.syncObj({_id: 1});
-        cache.removeObj({_id: 1});
-        expect(cache.getObjList().length).to.equal(0);
-        expect(Object.keys(cache.getObjHash()).length).to.equal(0);
-    });
-
-    it('should do nothing if removing an unexisting object', function(){
-        cache.removeObj({_id: 1});
-        expect(cache.getObjList().length).to.equal(0);
-        expect(Object.keys(cache.getObjHash()).length).to.equal(0);
-    });
-
-    it('should sync multiple objects', function(){
-        cache.syncMultipleObjs([{_id: 1}, {_id: 2}, {_id: 3}]);
-
-        expect(cache.getObjList().length).to.equal(3);
-        expect(Object.keys(cache.getObjHash()).length).to.equal(3);
-    });
-
-    it('should not append the same object in a array', function(){
-        cache.syncMultipleObjs([{_id: 1}, {_id: 2}, {_id: 3}, {_id: 1}]);
-
-        expect(cache.getObjList().length).to.equal(3);
-        expect(Object.keys(cache.getObjHash()).length).to.equal(3);
-    });
-
-    it('should modify the object on a preSync middleware', function(){
-        cache.preSync(function(obj) {
-            obj.state = 'custom';
-            return obj;
+    describe('.getObjList', () => {
+        it('should return an array', () => {
+            expect(cache.getObjList()).to.be.instanceOf(Array);
+            expect(cache.getObjList().length).to.equal(0);
         });
-        cache.syncObj({_id: 1});
-
-        expect(cache.getObj(1).state).to.equal('custom');
     });
 
-    it('should modify the object on multiple preSync middlewares', () => {
-        cache.preSync(function(obj) {
-            obj.state = 'custom';
-            return obj;
-        });
-        cache.preSync(function(obj) {
-            obj.currentDate = new Date();
-            return obj;
-        });
-        cache.syncObj({_id: 1});
+    describe('.syncObj', () => {
 
-        expect(cache.getObj(1).state).to.equal('custom');
-        expect(cache.getObj(1).currentDate).to.be.a('Date');
-    });
-
-    it('should notify a postSync event', () => {
-        cache.postSync((obj) => {
-            expect(obj._id).to.equal(1);
-        });
-        cache.syncObj({_id: 1});
-    });
-
-    it('should notify a postSync event after a preSync modifier', () => {
-        cache.preSync(function(obj) {
-            obj.state = 'custom';
-            return obj;
+        it('should cache a new object to the list and hash', () => {
+            cache.syncObj({_id: 1});
+            expect(cache.getObjList().length).to.equal(1);
+            expect(cache.getObj(1)._id).to.equal(1);
+            expect(Object.keys(cache.getObjHash()).length).to.equal(1);
         });
 
-        cache.postSync((obj) => {
-            expect(obj._id).to.equal(1);
-            expect(obj.state).to.equal('custom');
+        it('should not append the same object to the list and hash', () => {
+            cache.syncObj({_id: 1});
+            expect(cache.getObjList().length).to.equal(1);
+            expect(cache.getObj(1)._id).to.equal(1);
+
+            cache.syncObj({_id: 1, name:'Lucas'});
+            expect(cache.getObjList().length).to.equal(1);
+            expect(cache.getObj(1)._id).to.equal(1);
+            expect(cache.getObj(1).name).to.equal('Lucas');
+            expect(Object.keys(cache.getObjHash()).length).to.equal(1);
         });
-        cache.syncObj({_id: 1});
+
     });
+
+    describe('.removeObj', () => {
+        it('should remove an object from the list and hash', () => {
+            cache.syncObj({_id: 1});
+            cache.removeObj({_id: 1});
+            expect(cache.getObjList().length).to.equal(0);
+            expect(Object.keys(cache.getObjHash()).length).to.equal(0);
+        });
+
+        it('should do nothing if removing an unexisting object', () => {
+            cache.removeObj({_id: 1});
+            expect(cache.getObjList().length).to.equal(0);
+            expect(Object.keys(cache.getObjHash()).length).to.equal(0);
+        });
+    });
+
+    describe('.syncMultipleObjs', () => {
+
+        it('should sync multiple objects', () => {
+            cache.syncMultipleObjs([{_id: 1}, {_id: 2}, {_id: 3}]);
+
+            expect(cache.getObjList().length).to.equal(3);
+            expect(Object.keys(cache.getObjHash()).length).to.equal(3);
+        });
+
+        it('should not append the same object in a array', () => {
+            cache.syncMultipleObjs([{_id: 1}, {_id: 2}, {_id: 3}, {_id: 1}]);
+
+            expect(cache.getObjList().length).to.equal(3);
+            expect(Object.keys(cache.getObjHash()).length).to.equal(3);
+        });
+
+    });
+
+    describe('.preSync', () => {
+        it('should modify the object on a preSync middleware',() => {
+            cache.preSync(function(obj) {
+                obj.state = 'custom';
+                return obj;
+            });
+            cache.syncObj({_id: 1});
+
+            expect(cache.getObj(1).state).to.equal('custom');
+        });
+
+        it('should modify the object on multiple preSync middlewares', () => {
+            cache.preSync(function(obj) {
+                obj.state = 'custom';
+                return obj;
+            });
+            cache.preSync(function(obj) {
+                obj.currentDate = new Date();
+                return obj;
+            });
+            cache.syncObj({_id: 1});
+
+            expect(cache.getObj(1).state).to.equal('custom');
+            expect(cache.getObj(1).currentDate).to.be.a('Date');
+        });
+    });
+
+    describe('.postSync', () => {
+        it('should notify a postSync event', () => {
+            cache.postSync((obj) => {
+                expect(obj._id).to.equal(1);
+            });
+            cache.syncObj({_id: 1});
+        });
+
+        it('should notify a postSync event after a preSync modifier', () => {
+            cache.preSync(function(obj) {
+                obj.state = 'custom';
+                return obj;
+            });
+
+            cache.postSync((obj) => {
+                expect(obj._id).to.equal(1);
+                expect(obj.state).to.equal('custom');
+            });
+            cache.syncObj({_id: 1});
+        });
+    });
+
 });
 
 describe('Proxy', () => {
